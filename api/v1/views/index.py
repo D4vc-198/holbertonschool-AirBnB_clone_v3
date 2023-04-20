00ecf950c681356e27b5e7942df6d975cf43554b
d4vc-198 <contactodiegoavc@gmail.com>
2023-04-20 15:08:54 -0600

    Views fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""
-Module index:
-   url_prefix = '/api/v1'
-   routes:
-       - '/status'
-"""
+""" file index """
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-#  Add views to app_views  using the route decorator
-@app_views.route('/status')
-def status():
-    """ Method status """
-    return jsonify({"status": "OK"})
+@app_views.route("/status", strict_slashes=False)
+def app_views_status():
+    """ return status """
+    statusok = {"status": "OK"}
+    return jsonify(statusok)
 
 
-@app_views.route('/stats')
-def stats():
-    """ Method stast """
-    return jsonify(
-        {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
-        }
-    )
+@app_views.route("/stats", strict_slashes=False)
+def endpoint():
+    """ and endpoint that retrieves the number of each objects by type """
+
+    # joining the classes with our count method
+    clases = {"amenities": storage.count(Amenity),
+              "cities": storage.count(City),
+              "places": storage.count(Place),
+              "reviews": storage.count(Review),
+              "states": storage.count(State),
+              "users": storage.count(User)
+              }
+    return clases
