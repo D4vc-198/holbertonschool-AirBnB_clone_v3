d5f08e815ce0454cd12ad71bb001d6200aa0c7cb
d4vc-198 <contactodiegoavc@gmail.com>
2023-04-20 15:19:56 -0600

    status fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-""" file index """
-
+"""Flask application that retrieves information"""
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
-def app_views_status():
-    """ return status """
-    statusok = {"status": "OK"}
-    return jsonify(statusok)
+def status():
+    """Returns the app status"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route("/status", strict_slashes=False)
-def endpoint():
-    """ and endpoint that retrieves the number of each objects by type """
-
-    # joining the classes with our count method
-    clases = {"amenities": storage.count(Amenity),
-              "cities": storage.count(City),
-              "places": storage.count(Place),
-              "reviews": storage.count(Review),
-              "states": storage.count(State),
-              "users": storage.count(User)
-              }
-    return clases
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def stats():
+    """Retrieves the number of objects per each type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+    import json
+    dic = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    json_dict = json.dumps(dic, indent=2)
+    return json_dict
